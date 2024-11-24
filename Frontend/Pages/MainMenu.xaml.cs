92455bbbe09bc60f5ca90203a9792e0ffe536497
andr3i-f <105520459+andr3i-f@users.noreply.github.com>
2024-11-23 20:39:25 -0800

    Adding main menu (#22)
    Co-authored-by: BuzzerBeaterClutch <114624256+BuzzerBeaterClutch@users.noreply.github.com>

---
+++ Frontend/Pages/MainMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Common;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuniorProject
+{
+	/// <summary>
+	/// Interaction logic for Simulation.xaml
+	/// </summary>
+	public partial class MainMenu : Page
+	{
+		public MainMenu() { 
+			InitializeComponent();
+		}
+
+        private void StartSimulation(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Simulation());
+        }
+
+        private void Catalog(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Other Simulations";
+            string caption = "Coming Soon";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
+
+        private void Settings(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Settings";
+            string caption = "Coming Soon";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
+    }
+}
