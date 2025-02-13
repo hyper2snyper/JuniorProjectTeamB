b4c3badb0907c3964c6422881cd0216de214875d
BuzzerBeaterClutch <114624256+BuzzerBeaterClutch@users.noreply.github.com>
2025-02-12 20:21:30 -0800

    2nd Times the charm
    Hopefully no conflicts

--- Frontend/Pages/MapCreation.xaml.cs
+++ Frontend/Pages/MapCreation.xaml.cs
@@ -108,14 +108,11 @@ namespace JuniorProject.Frontend.Pages
             NavigationService.Navigate(new Loading());
         }
 
-<<<<<<< HEAD
         private void SimSettings_Click(object sender, RoutedEventArgs e)
         {
             SimulationSettings simSettings = new SimulationSettings();
             simSettings.Show();
             Console.WriteLine("SimSettings Windows");
         }
-=======
->>>>>>> main
     }
 }
