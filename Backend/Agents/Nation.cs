17230fc36cc7ca8ab0e6afbb138e0835e550cc25
Kaeden Grubb <kaeden.grubb@oit.edu>
2025-02-14 21:32:43 -0800

    Nation economy designed.

--- Backend/Agents/Nation.cs
+++ Backend/Agents/Nation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JuniorProject.Backend.WorldData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace JuniorProject.Backend.Agents
 
         List<Unit> units = new List<Unit>();
         List<Building> buildings = new List<Building>();
+        EconomyManager economy;
 
         public Nation()
         {
