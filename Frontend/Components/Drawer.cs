082a711862c2b259868a2aca6ce7e668f2c4e6dc
Andrei <andrei.florea003@gmail.com>
2025-02-16 18:38:16 -0800

    added spritesheet with images and json file for spritesheet data

--- Frontend/Components/Drawer.cs
+++ Frontend/Components/Drawer.cs
@@ -9,9 +9,16 @@ using Controls = System.Windows.Controls;
 using Colors = System.Drawing;
 using System.Windows.Controls;
 using System.Windows.Media;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace JuniorProject.Frontend.Components
 {
+    public class SpriteInfo
+    {
+        public int x1, y1, width, height;
+    }
+
     public class VectorPoint
     {
         public int x, y;
@@ -32,11 +39,19 @@ namespace JuniorProject.Frontend.Components
         private Dictionary<string, Drawable> drawables;
         private Dictionary<(int, int), string> drawableGridLocations;
 
+        string spriteSheetPath = $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\SpriteSheet.png";
+        string jsonPath = $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\sprites.json";
+        public Dictionary<string, SpriteInfo> sprites { get; set; }
+        Bitmap spriteSheet;
+
         public Drawer(ref Canvas mapCanvas)
         {
             Canvas = mapCanvas;
             drawables = new Dictionary<string, Drawable>();
             drawableGridLocations = new Dictionary<(int, int), string>();
+            string jsonData = File.ReadAllText(jsonPath);
+            sprites = JsonConvert.DeserializeObject<Dictionary<string, SpriteInfo>>(jsonData);
+            spriteSheet = new Bitmap(spriteSheetPath);
         }
 
         public void Initialize()
@@ -68,6 +83,21 @@ namespace JuniorProject.Frontend.Components
                     Debug.Print("Could not find image in drawables");
                 }
             }
+        }
+
+        public Bitmap extractFromSprite(string name)
+        {
+            if (sprites.TryGetValue(name, out SpriteInfo targetSprite))
+            {
+                Rectangle section = new Rectangle(targetSprite.x1, targetSprite.y1, targetSprite.width, targetSprite.height);
+
+                return spriteSheet.Clone(section, spriteSheet.PixelFormat);
+            }
+            else
+            {
+                Debug.Print(String.Format("ERROR! Cannot find sprite {0:S}", name));
+            }
+            return null;
         }
 
         public void ClearCanvas()
@@ -104,25 +134,101 @@ namespace JuniorProject.Frontend.Components
             {
                 AddBitmapToCanvas("MainMap", worldBitmap);
                 AddBitmapToCanvas("Grid", GetGridlines());
-                AddImageToCanvas("TestSpriteRed_1", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\TestSpriteRed.png", 3, 2);
-                AddImageToCanvas("TestSpriteYellow_1", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\TestSpriteYellow.png", 10, 15);
+
+                /*  vvvv FOR DEBUGGING/TESTING IMAGE SOURCES BELOW vvvv   */
+                //AddImageToCanvas("TestSpriteRed_1", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\TestSpriteRed.png", 3, 2);
+                //AddImageToCanvas("TestSpriteYellow_1", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\TestSpriteYellow.png", 10, 15);
+
+                //AddImageToCanvas("YellowKingdom", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\YellowCastle.png", 3, 3);
+                //AddImageToCanvas("YellowSoldier", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\YellowSoldier.png", 3, 4);
+                //AddImageToCanvas("YellowArcher", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\YellowArcher.png", 3, 5);
+                //AddImageToCanvas("YellowVillage", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\YellowHouse.png", 3, 6);
+
+                //AddImageToCanvas("GreenKingdom", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\GreenCastle.png", 4, 3);
+                //AddImageToCanvas("GreenSoldier", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\GreenSoldier.png", 4, 4);
+                //AddImageToCanvas("GreenArcher", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\GreenArcher.png", 4, 5);
+                //AddImageToCanvas("GreenVillage", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\GreenHouse.png", 4, 6);
+
+                //AddImageToCanvas("RedKingdom", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\RedCastle.png", 5, 3);
+                //AddImageToCanvas("RedSoldier", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\RedSoldier.png", 5, 4);
+                //AddImageToCanvas("RedArcher", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\RedArcher.png", 5, 5);
+                //AddImageToCanvas("RedVillage", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\RedHouse.png", 5, 6);
+
+                //AddImageToCanvas("WheatFarm", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\WheatFarm.png", 6, 3);
+                //AddImageToCanvas("Mine", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\Mine.png", 6, 4);
+
+                //AddImageToCanvas("Bread", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\FoodResource.png", 7, 3);
+                //AddImageToCanvas("Gold", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\GoldResource.png", 7, 4);
+                //AddImageToCanvas("Wood", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\WoodResource.png", 7, 5);
+                //AddImageToCanvas("Stone", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\StoneResource.png", 7, 6);
+                //AddImageToCanvas("Iron", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\IronResource.png", 7, 7);
+                //AddImageToCanvas("SwordUnit", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\SoldierUnitResource.png", 7, 8);
+                //AddImageToCanvas("ArcherUnit", $"{Properties.Resources.ProjectDir}\\Frontend\\Images\\Sprites\\IndividualImages\\ArcherUnitResource.png", 7, 9);
+
+                /*  vvvv FOR DEBUGGING/TESTING IMAGE SPRITES BELOW vvvv   */
+                //AddBitmapToCanvas("YellowCastle", extractFromSprite("YellowCastle"), 3, 3);
+                //AddBitmapToCanvas("YellowSoldier", extractFromSprite("YellowSoldier"), 3, 4);
+                //AddBitmapToCanvas("YellowArcher", extractFromSprite("YellowArcher"), 3, 5);
+                //AddBitmapToCanvas("YellowHouse", extractFromSprite("YellowHouse"), 3, 6);
+
+                //AddBitmapToCanvas("RedCastle", extractFromSprite("RedCastle"), 4, 3);
+                //AddBitmapToCanvas("RedSoldier", extractFromSprite("RedSoldier"), 4, 4);
+                //AddBitmapToCanvas("RedArcher", extractFromSprite("RedArcher"), 4, 5);
+                //AddBitmapToCanvas("RedHouse", extractFromSprite("RedHouse"), 4, 6);
+
+                //AddBitmapToCanvas("GreenCastle", extractFromSprite("GreenCastle"), 5, 3);
+                //AddBitmapToCanvas("GreenSoldier", extractFromSprite("GreenSoldier"), 5, 4);
+                //AddBitmapToCanvas("GreenArcher", extractFromSprite("GreenArcher"), 5, 5);
+                //AddBitmapToCanvas("GreenHouse", extractFromSprite("GreenHouse"), 5, 6);
+
+                //AddBitmapToCanvas("FoodResource", extractFromSprite("FoodResource"), 6, 3);
+                //AddBitmapToCanvas("Gold", extractFromSprite("GoldResource"), 6, 4);
+                //AddBitmapToCanvas("Wood", extractFromSprite("WoodResource"), 6, 5);
+                //AddBitmapToCanvas("Stone", extractFromSprite("StoneResource"), 6, 6);
+                //AddBitmapToCanvas("Iron", extractFromSprite("IronResource"), 6, 7);
+                //AddBitmapToCanvas("Soldier", extractFromSprite("SoldierResource"), 6, 8);
+                //AddBitmapToCanvas("Archer", extractFromSprite("ArcherResource"), 6, 9);
+
+                //AddBitmapToCanvas("WheatFarm", extractFromSprite("WheatFarm"), 7, 3);
+                //AddBitmapToCanvas("Mine", extractFromSprite("Mine"), 7, 4);
+
                 drawables["Grid"].shouldDraw = false;
                 drawables["Grid"].image.Opacity = 0.2;
             }
             PopulateCanvas();
         }
 
-        public void AddBitmapToCanvas(string name, Bitmap worldBitmap, int x = 0, int y = 0)
+        public void AddBitmapToCanvas(string name, Bitmap bitmap)
         {
             Controls.Image img = new Controls.Image
             {
-                Width = worldBitmap.Width,
-                Height = worldBitmap.Height,
-                Source = TransferToWriteableBitmap(worldBitmap)
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+                Source = TransferToWriteableBitmap(bitmap)
             };
             if (!drawables.TryAdd(name, new Drawable(img, true, name)))
             {
                 Debug.Print(String.Format("!!!ERROR: Cannot add {0:S} to drawables", name));
+            }
+        }
+
+        public void AddBitmapToCanvas(string name, Bitmap bitmap, int x, int y)
+        {
+            string imageSource = "SpriteSheet";
+            Controls.Image img = new Controls.Image
+            {
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+                Source = TransferToWriteableBitmap(bitmap)
+            };
+            VectorPoint p = ConvertGridPositionToPixels(x, y);
+            if (!drawables.TryAdd(name, new Drawable(img, true, name, imageSource, p.x, p.y, x, y)))
+            {
+                Debug.Print(String.Format("!!!ERROR: Cannot add {0:S} to drawables", name));
+            }
+            if (!drawableGridLocations.TryAdd((x, y), name))
+            {
+                Debug.Print(String.Format("!!!ERROR: Cannot add {0:S} to drawableGridLocations", name));
             }
         }
 
@@ -155,8 +261,8 @@ namespace JuniorProject.Frontend.Components
         public VectorPoint ConvertGridPositionToPixels(int x, int y)
         {
             // based on current Sprite being 20x20 sprites
-            int xPixelWidth = x * tileSize + (tileSize - 20);
-            int yPixelWidth = y * tileSize + (tileSize - 20);
+            int xPixelWidth = x * tileSize;
+            int yPixelWidth = y * tileSize;
             return new VectorPoint(xPixelWidth, yPixelWidth);
         }
 
