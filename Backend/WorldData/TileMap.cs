677d7cda4c273fee108c0c571b378b438840a1e6
Kaeden Grubb <kaeden.grubb@oit.edu>
2025-02-24 08:04:06 -0800

    Altered the resource data in biomes to be a dictionary instead of dual lists. Also changed the functions for getting resource data from tiles accordingly.

--- Backend/WorldData/TileMap.cs
+++ Backend/WorldData/TileMap.cs
@@ -227,7 +227,7 @@ namespace JuniorProject.Backend.WorldData
             ClientCommunicator.RegisterData<TileMap>("TileMap", this);
         }
 
-        List<string> GetTileResource(int xPos, int yPos)
+        int GetTileResource(int xPos, int yPos, string resourceType)
         {
             string primaryBiome = "";
             foreach (var item1 in tiles[xPos, yPos].terrainPercentages)
@@ -238,21 +238,7 @@ namespace JuniorProject.Backend.WorldData
                     else if (item2.Value > item1.Value) primaryBiome = item2.Key;
                 }
             }
-            return (map.GetBiomeResources(primaryBiome));
-        }
-
-        List<int> GetTileRate(int xPos, int yPos)
-        {
-            string primaryBiome = "";
-            foreach (var item1 in tiles[xPos, yPos].terrainPercentages)
-            {
-                foreach (var item2 in tiles[xPos, yPos].terrainPercentages)
-                {
-                    if (item1.Value > item2.Value) primaryBiome = item1.Key;
-                    else if (item2.Value > item1.Value) primaryBiome = item2.Key;
-                }
-            }
-            return (map.GetBiomeRate(primaryBiome));
+            return (map.GetBiomeResources(primaryBiome, resourceType));
         }
     }
 }
